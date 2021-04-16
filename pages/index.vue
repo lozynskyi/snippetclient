4d90bbea83fd5343a100195537ea3467f611d627
Slayter <slayter007@gmail.com>
2021-04-16 23:19:09 +0300

    Update snippet page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-    <h1 class="font-medium text-4xl">Snippets</h1>
+  <div class="container mt-16">
+    <h1 class="font-medium text-4xl">Home page</h1>
     <p>A little more paragraph text</p>
   </div>
 </template>
