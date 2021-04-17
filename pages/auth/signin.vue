83db913e979197129be8c4c5cfb543d1ab7e5049
Slayter <slayter007@gmail.com>
2021-04-17 11:27:41 +0300

    Add sign form

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container mt-16">
+    <div class="flex flex-col items-center">
+      <h1 class="text-3xl text-gray-700 font-medium mb-10"> Hello </h1>
+    </div>
     <a href="" @click.prevent="submit">sign in</a>
   </div>
 </template>
