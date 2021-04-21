d6448107b75f13c6c1705bfe976f1ad9c0907511
Slayter <slayter007@gmail.com>
2021-04-21 12:08:10 +0300

    Add list

--- components/global/TheFooter.vue
+++ components/global/TheFooter.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    footer
+  <div class="mt-24 mb-12">
+    <div class="container flex flex-wrap lg:flex-nowrap justify-between">
+      <ul class="flex items-center mb-6 lg:mb-0">
+        <li class="mr-16">
+          <nuxt-link
+            class=""
+            :to="{name: 'index'}"
+          >
+
+          </nuxt-link>
+
+        </li>
+      </ul>
+
+    </div>
   </div>
 </template>
