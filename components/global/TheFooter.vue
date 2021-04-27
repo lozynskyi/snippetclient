d6aa9a653ae1e8f6277d56365cf0af348f691ae4
Slayter <slayter007@gmail.com>
2021-04-27 23:38:25 +0300

    Add right links

--- components/global/TheFooter.vue
+++ components/global/TheFooter.vue
@@ -9,10 +9,22 @@
           >
           Home
           </nuxt-link>
-          <div>
+          <div class="text-gray-500 text-sm">
             &copy; BoringBox Ltd 2021. All rights reserved.
           </div>
+        </li>
+      </ul>
 
+      <ul class="flex items-center">
+        <li class="mr-8">
+          <a
+            class="text-gray-500 text-sm"
+            href="https://github.com"
+            target="_blank"
+            rel="noopener"
+          >
+            github.com
+          </a>
         </li>
       </ul>
 
